9fb4c077951a178b91a3b410723fa9607bcea5e4
Nasim Uddin <23162353+thenasim@users.noreply.github.com>
2021-03-09 12:49:26 +0600

    basic ownership

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 // mod data_types;
 // mod functions;
 // mod control_flow;
-mod fibonacci;
+// mod fibonacci;
+mod ownership;
 
 fn main() {
     // variables::run();
     // data_types::run();
     // functions::run();
     // control_flow::run();
-    fibonacci::run();
+    // fibonacci::run();
+    ownership::run();
 }
